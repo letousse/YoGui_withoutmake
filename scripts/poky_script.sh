f2c539d0755dd5948a7d434e7cc4ed35987e1947
letousse <55704069+letousse@users.noreply.github.com>
2019-10-02 17:34:30 +0100

    Update poky_script.sh

--- scripts/poky_script.sh
+++ scripts/poky_script.sh
@@ -91,12 +91,12 @@ fi
 if [ $1 = "bitbake" ]
 then 	
 	cd poky
-	echo "BB_MIN_VERSION = "1.39.1"" > /meta/conf/sanity.conf
-	echo "SANITY_ABIFILE = "${TMPDIR}/abi_version"" >> /meta/conf/sanity.conf
-	echo "SANITY_VERSION ?= "1"" >> /meta/conf/sanity.conf
-	echo "LOCALCONF_VERSION  ?= "1"" >> /meta/conf/sanity.conf
-	echo "LAYER_CONF_VERSION ?= "7"" >> /meta/conf/sanity.conf
-	echo "SITE_CONF_VERSION  ?= "1"" >> /meta/conf/sanity.conf
+	echo "BB_MIN_VERSION = "1.39.1"" > meta/conf/sanity.conf
+	echo "SANITY_ABIFILE = "${TMPDIR}/abi_version"" >> meta/conf/sanity.conf
+	echo "SANITY_VERSION ?= "1"" >> meta/conf/sanity.conf
+	echo "LOCALCONF_VERSION  ?= "1"" >> meta/conf/sanity.conf
+	echo "LAYER_CONF_VERSION ?= "7"" >> meta/conf/sanity.conf
+	echo "SITE_CONF_VERSION  ?= "1"" >> meta/conf/sanity.conf
 	source oe-init-build-env $2
 	cd $2
 	bitbake core-image-base
